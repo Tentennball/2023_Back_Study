5a62559e075db08054110202a244867962f2de07
hotteok00 <hojun7889@gmail.com>
2023-07-20 14:55:04 +0900

    Feat: jwt verify error

---
+++ Week1/hojun/app.js
@@ -0,0 +1,100 @@
+// node_modules 에 있는 express 관련 파일을 가져온다.
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+// env
+const dotenv = require('dotenv')
+dotenv.config()
+
+// private key
+const privateKey = process.env.KEY
+
+// connect my_db
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'back_study'
+}) // in back_study, TABLE : users, sessions
+
+// node_modules 에 있는 jsonwebtoken 관련 파일을 가져온다.
+const jwt = require("jsonwebtoken")
+
+// 4000 포트로 서버 오픈
+app.listen(4000, function () {
+    console.log("start! express server on port 4000")
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/main.html")
+})
+
+// localhost:3000//v1/index 브라우저에 res.sendFile() 내부의 파일이 띄워진다.
+app.get('/v1/index', function (req, res) {
+    res.sendFile(__dirname + "/v1/index.html")
+})
+
+app.post('/v1/index', function (req, res) {
+    res.send("hi postman")
+})
+
+app.post('/v1/register', async function (req, res) {
+    const reqBody = req.body
+
+    let flag
+    const setFlag = (value) => flag = value
+
+    connection.connect()
+
+    const sql_search = `SELECT * FROM users WHERE email LIKE '${reqBody.email}'`
+    connection.query(sql_search, (error, results, fields) => {
+        if (error) throw error
+        else {
+            console.log(results)
+            if (results.length === 0) setFlag(true)
+            else setFlag(false)
+        }
+    })
+
+    const sql_insert = `INSERT INTO users (email, password) VALUES ('${reqBody.email}', '${reqBody.password}')`
+    connection.query(sql_insert, (error, results, fields) => {
+        if (error) console.log(error)
+        else {
+            if (flag) res.send('Register Complete')
+            else res.send('Already Registerd')
+        }
+    })
+
+    connection.end()
+})
+
+app.post('/v1/login/jwtLogin', function (req, res) {
+    const reqBody = req.body
+
+    connection.connect()
+
+    const sql_login = `SELECT * FROM users WHERE email LIKE '${reqBody.email}' AND password LIKE '${reqBody.password}'`
+    connection.query(sql_login, (error, results, fields) => {
+        if (error) console.log(error)
+        else {
+            if (results.length === 0) res.send('Invalid email or password')
+            else {
+                const token = jwt.sign({ email: results.email }, privateKey)
+                res.header('authorization', token).send('Login Complete!')
+            }
+        }
+    })
+
+    connection.end()
+})
+
+app.post('/v1/login/jwtVerify', function (req, res) {
+    const reqHead = req.headers
+
+    const verified = jwt.verify(reqHead.authorization, privateKey)
+    res.send(verified)
+})
+
+// connection.end()
