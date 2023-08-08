c56827e95b763d6805c1a2f826d4d0c72d948a0b
jung-min-ju <minju9711@naver.com>
2023-08-08 13:29:09 +0900

    [add] week3

---
+++ Week3/minju/Domain/Seed.java
@@ -0,0 +1,13 @@
+package week3.seed.Domain;
+
+public class Seed {
+    private int seed;
+
+    public int getSeed() {
+        return seed;
+    }
+
+    public void setSeed(int seed) {
+        this.seed = seed;
+    }
+}
